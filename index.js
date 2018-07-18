2fa7c81bcce3e26ed95cb4c76b0444ee97824847
Emily Jirles <emily.jirles@gmail.com>
2018-07-18 05:18:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 $(document).ready(function (){
+  Handlebars.registerPartial('authorDetailsPartial', $('#author-details-partial'));
+
+  function searchRepositories(){
+    const termsHTML = $('#searchTerms')[0].value;
+    const searchTerms = termsHTML.split(' ').join('+');
+    const req = $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(response){
+      //put response through templates, pass off to results div
+      const repositoriesTemplate = $('#repositories-template')
+      $('#results').html(template)
+    });
+  };
 });
