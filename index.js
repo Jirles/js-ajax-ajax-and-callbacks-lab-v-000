4730a6dbd1d561ac2dd247f144aebfebe24b9532
Emily Jirles <emily.jirles@gmail.com>
2018-07-18 05:25:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 $(document).ready(function (){
   Handlebars.registerPartial('authorDetailsPartial', $('#author-details-partial'));
 
-  function searchRepositories(){
-    const termsHTML = $('#searchTerms')[0].value;
-    const searchTerms = termsHTML.split(' ').join('+');
-    const req = $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(response){
-      //put response through templates, pass off to results div
-      const tempateHTML = $('#repositories-template')[0].innerHTML;
-      const template = Handlebars.compile(templateHTML);
-      const repositories = template(response);
-      $('#results').html(repositories);
-    });
-  };
 });
+
+function searchRepositories(){
+  const termsHTML = $('#searchTerms')[0].value;
+  const searchTerms = termsHTML.split(' ').join('+');
+  const req = $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(response){
+    //put response through templates, pass off to results div
+    const tempateHTML = $('#repositories-template')[0].innerHTML;
+    const template = Handlebars.compile(templateHTML);
+    const repositories = template(response);
+    $('#results').html(repositories);
+  });
+};
