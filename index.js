917d2fe6a0231d6ea32519949b10ce2fb3d2cfc1
Emily Jirles <emily.jirles@gmail.com>
2018-07-18 05:45:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $(document).ready(function (){
 function searchRepositories(){
   const termsHTML = $('#searchTerms')[0].value;
   const searchTerms = termsHTML.split(' ').join('+');
-  const req = $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(response){
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(response){
     //put response through templates, pass off to results div
     const templateHTML = $('#repositories-template')[0].innerHTML;
     const template = Handlebars.compile(templateHTML);
@@ -15,6 +15,6 @@ function searchRepositories(){
   });
 };
 
-function showCommits(){
-
+function showCommits(element){
+  $.get(`https://api.github.com/repos/:owner/:repo/commits`)
 };
